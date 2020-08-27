553c63a50a890512629b1242c3380e0ce09efc21
Nir Kahana <nirkahana8@gmail.com>
2020-08-27 15:29:44 +0300

    server fixes 3

--- server/app.js
+++ server/app.js
@@ -1,10 +1,49 @@
 // const app = require('./index');
-const index = require('./index');
+// const app = require('./index');
 
-const port = 8080;
+// const port = 8080;
 
-index.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+// app.listen(port, () =>
+//   console.log(`Example app listening at http://localhost:${port}`)
+// );
+
+const fs = require('fs').promises;
+const express = require('express');
+
+const app = express();
+// const port = 8080; 
+app.use(express.json());
+// app.use(express.static('../client/build'));
+
+app.get('/api/tickets', async (req, res) => {
+  const ticketsJSON = await fs.readFile('./data.json');
+  const ticketsArray = JSON.parse(ticketsJSON);
+  if (req.query.searchText) {
+    const newTicketsArray = ticketsArray.filter((ticket, index) => ticketsArray[index].title.toLowerCase().includes(req.query.searchText.toLowerCase()));
+    res.send(newTicketsArray);
+  } else {
+    res.send(ticketsArray);
+  }
+});
+
+app.post('/api/tickets/:ticketId/done', async (req, res) => {
+  const ticketsJSON = await fs.readFile('./data.json');
+  const ticketsArray = JSON.parse(ticketsJSON);
+  const desiredTicket = ticketsArray.find((ticket) => ticket.id === req.params.ticketId);
+  desiredTicket.done = true;
+  res.send(ticketsArray);
+});
+
+app.post('/api/tickets/:ticketId/undone', async (req, res) => {
+  const ticketsJSON = await fs.readFile('./data.json');
+  const ticketsArray = JSON.parse(ticketsJSON);
+  const desiredTicket = ticketsArray.find((ticket) => ticket.id === req.params.ticketId);
+  desiredTicket.done = false;
+  res.send(ticketsArray);
+});
+
+// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`)); ////// ***
+
+module.exports = app;
 
 
--- server/index.js
+++ server/index.js
@@ -1,38 +1,52 @@
-const fs = require('fs').promises;
-const express = require('express');
+const app = require('./app');
 
-const app = express();
-// const port = 8080; 
-app.use(express.json());
-// app.use(express.static('../client/build'));
+const port = 8080;
 
-app.get('/api/tickets', async (req, res) => {
-  const ticketsJSON = await fs.readFile('./data.json');
-  const ticketsArray = JSON.parse(ticketsJSON);
-  if (req.query.searchText) {
-    const newTicketsArray = ticketsArray.filter((ticket, index) => ticketsArray[index].title.toLowerCase().includes(req.query.searchText.toLowerCase()));
-    res.send(newTicketsArray);
-  } else {
-    res.send(ticketsArray);
-  }
-});
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
 
-app.post('/api/tickets/:ticketId/done', async (req, res) => {
-  const ticketsJSON = await fs.readFile('./data.json');
-  const ticketsArray = JSON.parse(ticketsJSON);
-  const desiredTicket = ticketsArray.find((ticket) => ticket.id === req.params.ticketId);
-  desiredTicket.done = true;
-  res.send(ticketsArray);
-});
 
-app.post('/api/tickets/:ticketId/undone', async (req, res) => {
-  const ticketsJSON = await fs.readFile('./data.json');
-  const ticketsArray = JSON.parse(ticketsJSON);
-  const desiredTicket = ticketsArray.find((ticket) => ticket.id === req.params.ticketId);
-  desiredTicket.done = false;
-  res.send(ticketsArray);
-});
 
-// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`)); ////// ***
 
-module.exports = app;
+
+
+
+// const fs = require('fs').promises;
+// const express = require('express');
+
+// const app = express();
+// // const port = 8080; 
+// app.use(express.json());
+// // app.use(express.static('../client/build'));
+
+// app.get('/api/tickets', async (req, res) => {
+//   const ticketsJSON = await fs.readFile('./data.json');
+//   const ticketsArray = JSON.parse(ticketsJSON);
+//   if (req.query.searchText) {
+//     const newTicketsArray = ticketsArray.filter((ticket, index) => ticketsArray[index].title.toLowerCase().includes(req.query.searchText.toLowerCase()));
+//     res.send(newTicketsArray);
+//   } else {
+//     res.send(ticketsArray);
+//   }
+// });
+
+// app.post('/api/tickets/:ticketId/done', async (req, res) => {
+//   const ticketsJSON = await fs.readFile('./data.json');
+//   const ticketsArray = JSON.parse(ticketsJSON);
+//   const desiredTicket = ticketsArray.find((ticket) => ticket.id === req.params.ticketId);
+//   desiredTicket.done = true;
+//   res.send(ticketsArray);
+// });
+
+// app.post('/api/tickets/:ticketId/undone', async (req, res) => {
+//   const ticketsJSON = await fs.readFile('./data.json');
+//   const ticketsArray = JSON.parse(ticketsJSON);
+//   const desiredTicket = ticketsArray.find((ticket) => ticket.id === req.params.ticketId);
+//   desiredTicket.done = false;
+//   res.send(ticketsArray);
+// });
+
+// // app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`)); ////// ***
+
+// module.exports = app;
